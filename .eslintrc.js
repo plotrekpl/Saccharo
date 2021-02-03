25739d0589dba9e413e65870570b48152f71d7e4
Dominik Szczuka <szczuka27@gmail.com>
2021-02-03 20:30:00 +0100

    fixed configuration issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,62 +1,45 @@
 module.exports = {
-	'env': {
-		'es6': true,
-		'node': true,
-		'jest': true,
-	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:@typescript-eslint/eslint-recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:@typescript-eslint/recommended-requiring-type-checking',
-	],
-	'globals': {
-		'Atomics': 'readonly',
-		'SharedArrayBuffer': 'readonly'
-	},
-	'parser': '@typescript-eslint/parser',
-	'parserOptions': {
-		project: './tsconfig.json',
-		'ecmaFeatures': {
-			'jsx': true
-		},
-		'ecmaVersion': 2018,
-		'sourceType': 'module'
-	},
-	'plugins': [
-		'react',
-		'react-hooks',
-		'@typescript-eslint',
-		'prettier'
-	],
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		],
-		'no-empty-function': 'off',
-		'@typescript-eslint/no-empty-function': 'off',
-		'@typescript-eslint/no-empty-function': 'off',
-		'react/display-name': 'off',
-		'react/prop-types': 'off',
-		'prettier/prettier': 'error',
-	},
-	settings: {
-		react: {
-		  version: 'detect',
-		},
-	  },
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    indent: ['error', 'tab'],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
