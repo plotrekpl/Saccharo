c66e7e6221ac9877259dac0ac6669e0166f41e77
dominikszczuka <szczuka27@gmail.com>
2021-02-04 15:02:20 +0100

    TASK4-connectReduxAndSaga

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import { Provider } from 'react-redux';
+import store from './src/store/store';
 
 const App = () => {
   return (
-    <View>
-      <Text>QofSugar</Text>
-    </View>
+    <Provider store={store}>
+      <Text>Hello</Text>
+    </Provider>
+
   );
 };
 
