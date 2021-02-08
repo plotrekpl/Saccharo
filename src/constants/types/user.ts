0dd4f94a745c0bdc7926d294fd34c29872940bb9
jakub <jpilawka@concisesoftware.com>
2021-02-08 08:02:04 +0100

    1. Added comments

--- src/constants/types/user.ts
+++ src/constants/types/user.ts
@@ -3,12 +3,14 @@ export interface ICredentials {
   password: string;
 }
 
+/** use I prefix for all Interfaces if you want to follow convention */
 export interface User {
   uid: string;
   email: string;
   displayName: string;
 }
 
+/** use I prefix for all Interfaces if you want to follow convention */
 export interface UserResponse extends User {
   refreshToken: string;
   accessToken: string;
