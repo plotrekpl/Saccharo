61d4fbf4844c3301bf2d4b57a9a4bcebbddd2abc
dominikszczuka <szczuka27@gmail.com>
2021-02-09 08:15:32 +0100

    change user types

--- src/constants/types/user.ts
+++ src/constants/types/user.ts
@@ -1,20 +1,26 @@
-export interface ICredentials {
+export interface ILoginCredentials {
   email: string;
   password: string;
-  userName?: string;
+}
+
+export interface IRegisterCredentials extends ILoginCredentials {
+  name: string;
 }
 
 export interface IUser {
   uid: string;
   email: string;
+  name: string;
+  avatar?: string;
 }
 
-export interface IUserResponse extends IUser {
+export interface IUserResponse {
+  user: IUser;
+  auth: IAuth;
+}
+
+export interface IAuth {
   refreshToken: string;
   accessToken: string;
   expirationTime: number;
-}
-
-export interface IUserData {
-  userName: string;
 }
