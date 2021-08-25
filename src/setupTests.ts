ea2d3369e0e3a8ed5b7c632f5b5ec4c196738c96
plotrekhq <plotrekhqpl@gmail.com>
2021-08-25 17:35:40 +0200

    esllint promlem resolved

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,11 +1,11 @@
-jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
-const mockedNavigate = jest.fn();
+jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter')
+const mockedNavigate = jest.fn()
 jest.mock('@react-navigation/native', () => ({
-  useNavigation: () => ({ navigate: mockedNavigate }),
-}));
+  useNavigation: () => ({ navigate: mockedNavigate })
+}))
 
 jest.mock('react-i18next', () => ({
-  useTranslation: () => ({ t: (key: string) => key }),
-}));
+  useTranslation: () => ({ t: (key: string) => key })
+}))
 
 export {}
